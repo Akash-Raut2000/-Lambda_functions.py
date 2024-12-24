ecb5bd1380f85599b32d399da515704a5b95a25b
Akash Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-24 19:07:31 +0530

    Add files via upload

---
+++ Lambda_Functions.py
@@ -0,0 +1,12 @@
+# Lambda function to add two numbers
+add_numbers = lambda x, y: x + y
+
+# Using the lambda function
+result = add_numbers(5, 3)
+print("The result is:", result)
+
+
+Mult_Num = lambda a, b:a*b
+
+ans = Mult_Num(3,8)
+print(ans)
